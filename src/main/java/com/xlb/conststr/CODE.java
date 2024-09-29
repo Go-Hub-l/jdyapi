36ae06ba71a45339a4a5f54efa56bccba5c3b9f4
Go-Hub-l <18055427868@163.com>
2024-09-29 18:03:33 +0800

    [UPDATE][完成同步]

---
+++ src/main/java/com/xlb/conststr/CODE.java
@@ -0,0 +1,10 @@
+package com.xlb.conststr;
+
+
+/**
+ * 错误码链接:https://hc.jiandaoyun.com/open/api/explorer?api_key=corp.user.create
+ */
+public class CODE {
+    public static final int SUCCESS = 0;
+    public static final int USER_NOT_EXIST = 1010;
+}
